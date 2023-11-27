0beab2aab133ad83ca3723115918724d3ff378b7
Connor Armand Du Plooy <13156302+CDuPlooy@users.noreply.github.com>
2023-11-27 13:36:58 +0200

    Update setup.py
    fookit

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup, find_packages
 from pipenv.project import Project
-from pipenv.utils import convert_deps_to_pip
 from os import path
 from io import open
 
 here = path.abspath(path.dirname(__file__))
 pfile = Project(__file__).parsed_pipfile
-requirements = convert_deps_to_pip(pfile['packages'], r=False)
-print(requirements)
+
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -40,7 +38,6 @@ setup(
     keywords='reverse_engineering android xamarin apk',
     packages=find_packages(),
     python_requires='>=3.5',
-    install_requires=requirements,
     entry_points={
         'console_scripts': [
             'mono_unbundle=mono_unbundle.cli:cli'
